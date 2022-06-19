4fc81657a88e376742375d37aeb11162f9970c37
Filipe Bezerra <filipebzerra@gmail.com>
2022-06-19 13:04:20 -0300

    Recreate project using AS Bottom Navigation project template #43

--- app/src/main/java/com/github/filipebezerra/bible/verseoftheday/MainActivity.kt
+++ app/src/main/java/com/github/filipebezerra/bible/verseoftheday/MainActivity.kt
@@ -2,40 +2,34 @@ package com.github.filipebezerra.bible.verseoftheday
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil.setContentView
-import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.github.filipebezerra.bible.verseoftheday.databinding.ActivityMainBinding
+import com.github.filipebezerra.bible.verseoftheday.databinding.MainActivityBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: MainActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
-        val navController = findNavController(R.id.nav_host_fragment)
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_verse_history,
-                R.id.navigation_todays_verse
+                R.id.navigation_home, R.id.navigation_dashboard
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
-        binding.navView.setupWithNavController(navController)
-
-        binding.navView.getOrCreateBadge(binding.navView.menu.findItem(R.id.navigation_todays_verse).itemId)
-            .apply { isVisible = true }
-        navController.addOnDestinationChangedListener { _, destination, _ ->
-            changeTitleWhen(destination)
-        }
-    }
-
-    private fun changeTitleWhen(destination: NavDestination) {
-        when (destination.id) {
-            R.id.navigation_todays_verse -> supportActionBar?.title = getString(R.string.app_name)
-        }
+        navView.setupWithNavController(navController)
     }
 }
